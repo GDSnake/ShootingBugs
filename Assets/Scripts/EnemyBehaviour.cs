f0565d108acb8a6640b9a632cdc6d718aa26bcad
GDSnake <gon.n.delgado@gmail.com>
2018-08-08 18:16:09 +0100

    Simple enemy movement created

---
+++ Assets/Scripts/EnemyBehaviour.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyBehaviour : MonoBehaviour
+{
+
+    public float speed;
+	// Use this for initialization
+	void Start ()
+	{
+	    gameObject.GetComponent<Rigidbody2D>().velocity = new Vector2(speed, 0);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public Vector3 spawnValues;
+    public GameObject Enemy;
+	// Use this for initialization
+	void Start ()
+	{
+	    SpawnWaves();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+    void SpawnWaves() {
+        Vector3 spawnPosition = new Vector3(spawnValues.x,spawnValues.y,spawnValues.z);
+        Quaternion spawnDirection = Quaternion.identity;
+        Instantiate(Enemy,spawnPosition,spawnDirection);
+    }
+}
